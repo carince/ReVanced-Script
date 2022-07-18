ac30668285a1c7304c50b70dc4c47c3da6f201e0
Norikiru <norikiru.official@gmail.com>
2022-07-18 12:26:42 +0800

    app: use listr2, add downloads

--- index.js
+++ index.js
@@ -1,43 +1,102 @@
 import { execa } from 'execa'
 import figlet from 'figlet'
-import Listr from 'listr'
+import fs from 'fs'
+import { Listr } from 'listr2'
+import open from 'open'
 
 console.clear()
 console.log(figlet.textSync(`ReVanced-Script`))
 
-const main = new Listr([
-    {
-        title: 'Checking some stuff before we continue',
-        task: () => {
-            return new Listr([
-                {
-                    title: 'Do we have internet?',
-                    task: async () => {
-                        await execa('ping', ['-c 5', 'google.com'])
-                    }
-                },
-                {
-                    title: 'Is curl installed?',
-                    task: async () => {
-                        await execa('curl', ['--version'])
-                    }
-                },
-                {
-                    title: 'Is Zulu JDK installed?',
-                    task: async () => {
-                        const {stdout} = await execa('java', ['--version'])
+const main = new Listr(
+    [
+        {
+            title: `Checking some stuff before we continue...`,
+            task: () => new Listr(
+                [
+                    {
+                        title: 'Do we have internet?',
+                        task: async () => {
+                            await execa('ping', ['-c 5', 'google.com'])
+                        }
+                    },
+                    {
+                        title: 'Is curl installed?',
+                        task: async () => {
+                            await execa('curl', ['--version'])
+                        }
+                    },
+                    {
+                        title: 'Is Zulu JDK installed?',
+                        task: async () => {
+                            const { stdout } = await execa('java', ['--version'])
 
-                        if(stdout){
-                            const string = stdout
-                            if(string.includes(`Zulu17`)){
-                                throw new Error(`zulu17 is not installed`)
+                            if (stdout) {
+                                const string = stdout
+                                if (!string.includes(`Zulu17`)) {
+                                    await open('https://www.azul.com/downloads/?version=java-17-lts&package=jdk')
+                                    throw new Error(`Zulu17 is not installed, check your browser and download the file according to your OS.`)
+                                }
                             }
                         }
                     }
-                },
-            ], {concurrent: 1});
+                ],
+                {concurrent: true}
+            )
+        },
+        {
+            title: `Download needed packages...`,
+            task: () => new Listr(
+                [
+                    {
+                        title: `Do we already have the files downloaded?`,
+                        task: (ctx, task) => {
+                            const dlFiles = []
+                            if(!fs.existsSync(`./cli.jar`)) dlFiles.push(`cli`), ctx.dlCli = true
+                            if(!fs.existsSync(`./patches.jar`)) dlFiles.push(`patches`), ctx.dlPatches = true
+                            if(!fs.existsSync(`./integrations.apk`)) dlFiles.push(`integrations`), ctx.dlInteg = true
+
+                            if (dlFiles.length == 0) {
+                                task.output = `Detected all file(s), skipping download.`
+                            }
+
+                            if (dlFiles.length > 0) {
+                                task.skip(`Missing ${dlFiles.length} file(s), will be downloading the following: ${dlFiles}`)
+                            }
+                        },
+                        options: { persistentOutput: true }
+                    },
+                    {
+                        title: `CLI`,
+                        enabled: (ctx) => ctx.dlCli == true,
+                        task: async () => {
+                            await execa('curl', ['-ocli.jar', 'https://github.com/revanced/revanced-cli/releases/download/v2.5.3/revanced-cli-2.5.3-all.jar'])
+                        }
+                    },
+                    {
+                        title: `Patches`,
+                        enabled: (ctx) => ctx.dlPatches == true,
+                        task: async () => {
+                            await execa('curl', ['-opatches.jar', 'https://github.com/revanced/revanced-cli/releases/download/v2.5.3/revanced-cli-2.5.3-all.jar'])
+                        }
+                    },
+                    {
+                        title: `Integrations`,
+                        enabled: (ctx) => ctx.dlInteg == true,
+                        task: async () => {
+                            await execa('curl', ['-ointeg.apk', 'https://github.com/revanced/revanced-cli/releases/download/v2.5.3/revanced-cli-2.5.3-all.jar'])
+                        }
+                    },
+                ]
+            )
+        }
+    ],
+    {
+        concurrent: false,
+        rendererOptions: {
+            collapse: false,
+            collapseSkips: false
         }
     }
-]);
+)
 
-main.run()
+await main.run();
