0b2107952c6e775483ad00f0c523f976abc02f9d
Norikiru <norikiru.official@gmail.com>
2022-07-21 02:51:26 +0800

    app: drop listr2, use ora & inquirer instead.

--- index.js
+++ index.js
@@ -1,172 +1,246 @@
-import { execa, execaCommand } from 'execa'
 import figlet from 'figlet'
-import fs from 'fs'
-import { Listr } from 'listr2'
-import open from 'open'
-import enquirer from 'enquirer'
+import ora from 'ora'
+import { execa, execaCommand } from 'execa'
+import chalk from 'chalk'
+import inquirer from 'inquirer'
+import gradient from 'gradient-string'
 
-console.clear()
-console.log(figlet.textSync(`ReVanced-Script`))
+import { existsSync, mkdirSync } from 'fs'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
+const { textSync } = figlet
 
-const main = new Listr(
-    [
-        {
-            title: `Checking some stuff before we continue...`,
-            task: () => new Listr(
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const filesDir = join(__dirname, `rvs-files`)
+let ytName;
+let rvName;
+let patchArr = new Array();
+
+await console.clear()
+const rvGradient = gradient(['#bb2ea1', '#ebf92b'])
+console.log(rvGradient(textSync(`ReVanced-Script`)))
+
+async function main() {
+    console.log(chalk.bold.green(`Checks`))
+    async function mkdir() {
+        const spinner = ora(`Create a directory for our files to sit in...`).start()
+
+        if (!existsSync(filesDir)) {
+            mkdirSync(filesDir);
+            spinner.succeed(chalk.greenBright(`Created files directory.`))
+        } else {
+            spinner.warn(chalk.yellowBright(`Files directory already exists, ${chalk.italic(`skipping...`)}`))
+        }
+    } await mkdir()
+
+    async function checkJava() {
+        const spinner = ora(`Checking if Zulu JDK 17 is installed...`).start()
+
+        const { stdout } = await execa('java', ['--version']).catch(() => {
+            const errTxt = `Java is not installed.`
+            spinner.fail(chalk.bold.red(errTxt))
+            throw new Error(errTxt)
+        })
+
+        if (!stdout.includes(`Zulu`)) {
+            const errTxt = `Zulu is not installed.`
+            spinner.fail(chalk.bold.red(errTxt))
+            throw new Error(errTxt)
+        }
+
+        if (!stdout.includes(`Zulu17`)) {
+            const errTxt = `Zulu is installed but with a incorrect version.`
+            spinner.fail(chalk.bold.red(errTxt))
+            throw new Error(errTxt)
+        }
+
+        spinner.succeed(chalk.greenBright(`Zulu JDK is installed...`))
+    } await checkJava()
+
+    async function checkCurl() {
+        const spinner = ora(`Checking if curl is installed...`).start()
+
+        await execa('curl', [`--version`]).catch(() => {
+            const errTxt = `Curl is not installed.`
+            spinner.fail(chalk.bold.red(errTxt))
+            throw new Error(errTxt)
+        })
+
+        spinner.succeed(chalk.greenBright(`curl is installed...`))
+    } await checkCurl()
+
+    console.log(chalk.bold.green(`Downloads`))
+    async function dlPkg() {
+        await cli()
+        async function cli() {
+            const spinner = ora(`Downloading CLI...`).start()
+
+            if (existsSync(join(filesDir, `cli.jar`))) {
+                spinner.warn(chalk.yellowBright(`CLI already exists, ${chalk.italic(`skipping...`)}`))
+                return
+            }
+
+            const { stdout } = await execa('curl', ['-s', 'https://api.github.com/repos/revanced/revanced-cli/releases/latest'])
+            const json = JSON.parse(stdout)
+            const version = json[`tag_name`].substring(1)
+
+            await execa('curl',
                 [
-                    {
-                        title: 'Do we have internet?',
-                        task: async () => {
-                            await execa('ping', ['-c 1', 'google.com'])
-                        }
-                    },
-                    {
-                        title: 'Is curl installed?',
-                        task: async () => {
-                            await execa('curl', ['--version']).catch()
-                        }
-                    },
-                    {
-                        title: 'Is Zulu JDK installed?',
-                        task: async () => {
-                            const { stdout } = await execa('java', ['--version'])
-
-                            if (stdout) {
-                                const string = stdout
-                                if (!string.includes(`Zulu17`)) {
-                                    await open('https://www.azul.com/downloads/?version=java-17-lts&package=jdk')
-                                    throw new Error(`Zulu17 is not installed, check your browser and download the file according to your OS.`)
-                                }
-                            }
-                        }
-                    }
+                    '-Locli.jar',
+                    `https://github.com/revanced/revanced-cli/releases/download/v${version}/revanced-cli-${version}-all.jar`
                 ],
-                {concurrent: true}
+                {
+                    cwd: filesDir
+                }
             )
-        },
-        {
-            title: `Download needed packages...`,
-            task: () => new Listr(
+
+            spinner.succeed(chalk.greenBright(`CLI successfully downloaded...`))
+        }
+
+        await patches()
+        async function patches() {
+            const spinner = ora(`Downloading Patches...`).start()
+
+            if (existsSync(join(filesDir, `patches.jar`))) {
+                spinner.warn(chalk.yellowBright(`Patches already exists, ${chalk.italic(`skipping...`)}`))
+                return
+            }
+
+            const { stdout } = await execa('curl', ['-s', 'https://api.github.com/repos/revanced/revanced-patches/releases/latest'])
+            const json = JSON.parse(stdout)
+            const version = json[`tag_name`].substring(1)
+
+            await execa('curl',
                 [
-                    {
-                        title: `Do we already have the files downloaded?`,
-                        options: { persistentOutput: true },
-                        task: (ctx, task) => {
-                            const dlFiles = []
-                            if(!fs.existsSync(`./cli.jar`)) dlFiles.push(`cli`), ctx.dlCli = true
-                            if(!fs.existsSync(`./patches.jar`)) dlFiles.push(`patches`), ctx.dlPatches = true
-                            if(!fs.existsSync(`./integ.apk`)) dlFiles.push(`integrations`), ctx.dlInteg = true
-
-                            if (dlFiles.length == 0) {
-                                task.output = `Detected all file(s), skipping download.`
-                            }
-
-                            if (dlFiles.length > 0) {
-                                task.skip(`Missing ${dlFiles.length} file(s), will be downloading the following: ${dlFiles}`)
-                            }
-                        }
-                    },
-                    {
-                        title: `CLI`,
-                        enabled: (ctx) => ctx.dlCli == true,
-                        task: async (ctx, task) => {
-                            const { stdout } = await execa('curl', ['-s', 'https://api.github.com/repos/revanced/revanced-cli/releases/latest'])
-                            const json = JSON.parse(stdout)
-                            const version = json[`tag_name`].substring(1)
-
-                            await execa('curl', ['-Locli.jar', `https://github.com/revanced/revanced-cli/releases/download/v${version}/revanced-cli-${version}-all.jar`])
-                        }
-                    },
-                    {
-                        title: `Patches`,
-                        enabled: (ctx) => ctx.dlPatches == true,
-                        task: async () => {
-                            const { stdout } = await execa('curl', ['-s', 'https://api.github.com/repos/revanced/revanced-patches/releases/latest'])
-                            const json = JSON.parse(stdout)
-                            const version = json[`tag_name`].substring(1)
-
-                            await execa('curl', ['-Lopatches.jar', `https://github.com/revanced/revanced-patches/releases/download/v${version}/revanced-patches-${version}.jar`])
-                        }
-                    },
-                    {
-                        title: `Integrations`,
-                        enabled: (ctx) => ctx.dlInteg == true,
-                        task: async () => {
-                            await execa('curl', ['-Lointeg.apk', `https://github.com/revanced/revanced-integrations/releases/download/v0.27.0/app-release-unsigned.apk`])
-                        }
-                    },
-                ]
+                    '-Lopatches.jar',
+                    `https://github.com/revanced/revanced-patches/releases/download/v${version}/revanced-patches-${version}.jar`
+                ],
+                {
+                    cwd: filesDir
+                }
             )
-        },
-        {
-            title: `Configuring ReVanced options...`,
-            task: () => new Listr(
-                [
-                    {
-                        title: `YouTube APK`,
-                        retry: 3,
-                        task: async (ctx, task) => {
-                            const prompt = await task.prompt(
-                                {
-                                    type: 'Input',
-                                    message: 'What is your YouTube APK filename?',
-                                    footer: ctx.apkPrompt || 'example: yt.apk',
-                                    initial: 'yt.apk'
-                                }
-                            )
 
-                            if(!fs.existsSync(prompt)) {
-                                ctx.apkPrompt = `File doesnt exist. Maybe check your spelling?`
-                                throw new Error(`File doesnt exist.`)
+            spinner.succeed(chalk.greenBright(`Patches successfully downloaded...`))
+        }
+
+        await integ()
+        async function integ() {
+            const spinner = ora(`Downloading Integrations...`).start()
+
+            if (existsSync(join(filesDir, `integ.apk`))) {
+                spinner.warn(chalk.yellowBright(`Integrations already exists, ${chalk.italic(`skipping...`)}`))
+                return
+            }
+
+            await execa('curl',
+                [
+                    '-Lointeg.apk',
+                    `https://github.com/revanced/revanced-integrations/releases/download/v0.27.0/app-release-unsigned.apk`
+                ],
+                {
+                    cwd: filesDir
+                }
+            )
+
+            spinner.succeed(chalk.greenBright(`Integrations successfully downloaded...`))
+        }
+    } await dlPkg()
+
+    console.log(chalk.bold.green(`Configuration`))
+    async function configure() {
+        async function ytApk() {
+            await inquirer.prompt(
+                {
+                    type: `input`,
+                    name: `ytApk`,
+                    message: `What is your YouTube apk filename?\n  ${chalk.italic(`(file must be in the 'rvs-files' directory.)`)}\n  rvs-files: file://${filesDir}`,
+                    default: `yt.apk`,
+                    validate: function (input) {
+                        const done = this.async();
+                        async function checkFile() {
+                            if (!existsSync(join(filesDir, input))) {
+                                done(chalk.yellowBright(`⚠ File does not exist, please double check your spelling.`))
+                                return
                             } else {
-                                ctx.ytDir = prompt
+                                ytName = input
+                                done(null, true)
                             }
-                        }
-                    },
-                    {
-                        title: `ReVanced Patches`,
-                        task: async (ctx, task) => {
-                            const { stdout } = await execaCommand(`java -jar cli.jar -a ${ctx.ytDir} -c -l -o revanced.apk -b patches.jar -m integ.apk`)
-                
-                            let outputArr = stdout.toString().replace(/\r\n/g, '\n').split('\n')                      
-
-                            let nameArr = outputArr.map(line => {
-                                let cleaned = line.replaceAll('INFO:', '')
-                                    cleaned = cleaned.replaceAll('"', "'")
-                                    cleaned = cleaned.replaceAll(/\t/g, '   ')
-                                return cleaned
-                            })
-
-                            let choicesArr = []
-                            for (let name of nameArr) {
-                                await choicesArr.push(JSON.parse(`{"name": "${name}"}`))
-                            }
-
-                            const prompt = await task.prompt(
-                                {
-                                    type: 'MultiSelect',
-                                    name: `prompt`,
-                                    message: 'Select the patches you want installed onto ReVanced',
-                                    footer: 'Usage: Space for toggle, arrow keys for navigating, and Enter to confirm',
-                                    choices: choicesArr
-                                }
-                            )
-
-                            ctx.patchesArr = prompt.map(line => line.trim().split(' ')[0])
-                        }
+                        } checkFile();
                     }
-                ],
-                { concurrent: false, injectWrapper: { enquirer } }
+                }
             )
-        }
-    ],
-    {
-        concurrent: false,
-        rendererOptions: {
-            collapse: false,
-            collapseSkips: false
-        }
-    }
-)
+        } await ytApk()
 
-await main.run();
+        async function slctPatches() {
+            const { stdout } = await execaCommand(`java -jar cli.jar -a ${ytName} -c -l -o revanced.apk -b patches.jar -m integ.apk`,
+                {
+                    cwd: filesDir
+                }
+            )
+
+            let outputArr = stdout.toString().replace(/\r\n/g, '\n').split('\n')
+
+            let nameArr = outputArr.map(line => {
+                let cleaned = line.replaceAll('INFO:', '')
+                cleaned = cleaned.replaceAll('"', "'")
+                cleaned = cleaned.replaceAll(/\t/g, '   ')
+                return cleaned
+            })
+
+            let choicesArr = []
+            for (let name of nameArr) {
+                const value = name.trim().split(' ')[0]
+                await choicesArr.push(JSON.parse(`{"name": "${name}", "value": "${value}", "short": "${value}"}`))
+            }
+
+            await inquirer.prompt(
+                {
+                    type: `checkbox`,
+                    name: `patchArr`,
+                    message: `Select your preferred patches.`,
+                    pageSize: 15,
+                    choices: choicesArr,
+                    validate: function (input) {
+                        const done = this.async();
+                        async function check() {
+                            if (!input.length > 0) {
+                                done(chalk.yellowBright(`⚠ You must pick atleast one of the patches.`))
+                                return
+                            }
+                            patchArr = input
+                            done(null, true)
+                        } check();
+                    }
+                }
+            )
+        } await slctPatches()
+
+        async function apkName() {
+            await inquirer.prompt(
+                {
+                    type: `input`,
+                    name: `apkName`,
+                    message: `What would you like your ReVanced apk be named?\n  (spaces and '.apk' will be removed.)`,
+                    default: `revanced`,
+                    validate: function (input) {
+                        const done = this.async();
+                        async function check() {
+                            let name = input.replaceAll(` `, '')
+
+                            if (!name) {
+                                done(chalk.yellowBright(`⚠ Name cannot be empty`))
+                                return
+                            }
+                            if (name.endsWith(`.apk`)) {
+                                name.replace(`.apk`, ``)
+                            }
+
+                            rvName = name
+                            done(null, true)
+                        } check();
+                    }
+                }
+            )
+        } await apkName()
+    } await configure()
+} main();
